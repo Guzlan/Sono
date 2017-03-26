9887bd37c62573a6a9eac0f5db68795baca0b1e9
Zeyad Saleh <zeyadhazemsaleh@gmail.com>
2017-03-26 19:06:50 -0400

    Filtering, Biomusic Modifications, Minor UI changes
    - Implemented Moving average filter over a sample of 10 data points
    - Reattacking chords after 8 mesures
    - Added drums
    - Changed dynamics of music (chords louder than melody)
    - Changed the threshold for temperature and GSR

--- Sono/Biomusic.swift
+++ Sono/Biomusic.swift
@@ -26,29 +26,45 @@ class  Biomusic {
     var quadraticTime: [[Double]] = [[Double]](repeating: [0.0], count: 5)
     var linearTime: [[Double]] = [[Double]](repeating: [0.0], count: 5)
     
+    // Used to set up the matrices for first and second derivative calculations
     let readingsNumber = 5
+    
+    // Global vars for temperature calculation
     var currentTempReadings = 0
-    var currentGSRReadings = 0
+    var temperatureMatrixIndex = 0
+    var temperatureMovingAvgFilter: [Double] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     var temperatureReadings: [Double] = [0, 0, 0, 0, 0]
     var newTemperatureReading: Double = 0.0
+    var deltaTemp   : Double = 0.025
+    var currentTemp : Double = 36.4
+    var baselineTemp: Double = 0
+    
+    // Global vars for GSR calculation
+    var currentGSRReadings = 0
+    var GSRMatrixIndex = 0
+    var GSRMovingAvgFilter: [Double] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     var GSRReadings: [Double] = [0, 0, 0, 0, 0]
     var newGSRReading: Double = 0.0
+    var deltaEDA     : Double = 0.005
+    var baselineEDA : Double = 0
+    var currentEDA  : Double = 0.1
+    
+    // Global vars for HR
     var ibiIndex = 0
     var newHRReading: Double = 0.0
     var newTimerFrequency: Double = 1.0
-    
-    var deltaTemp   : Double = 0.2
-    var deltaEDA     : Double = 0.05
-    var baselineTemp: Double = 0
-    var baselineEDA : Double = 0
-    var currentTemp : Double = 36.4
-    var currentEDA  : Double = 0.1
-    var currentChord: Int    = 0
     var timerFrequency:Double = 1.0
+    
+    
+    var currentChord: Int    = 0
     var pluckPosition   = 0.5
     var mandolin2PluckPosition = 0.2
     var majorScale = [0,2,4,5,7,9,11]
     var scaleIndex = 0
+    var melodyVelocity = 50
+    var chordsVelocity = 127
+    let reattackDuration = 8
+    var lastTimeSinceReattack = 0
     
     var readingsIndex = 0
     
@@ -76,7 +92,7 @@ class  Biomusic {
         mandolin.presetLargeResonantMandolin()
         let mandolinReverb = AKCostelloReverb(mandolin)
         
-        mandolinReverb.feedback = 0.9
+        mandolinReverb.feedback = 0.99
         
         mandolin2.detune = 1
         mandolin2.bodySize = 1.95
@@ -88,10 +104,11 @@ class  Biomusic {
         mandolin2Reverb.feedback = 2
         
         // Prepare Output
-        let mix = AKMixer(kick, snare, mandolinReverb, mandolin2Reverb, clarinet)
-        let reverb = AKReverb(mix)
+        let music = AKMixer(mandolinReverb, mandolin2Reverb, clarinet)
+        let reverb = AKReverb(music)
+        let mix = AKMixer(kick, snare, reverb)
         
-        AudioKit.output = reverb
+        AudioKit.output = mix
         AudioKit.start()
         
         // Effects
@@ -116,13 +133,12 @@ class  Biomusic {
         // Start Timers
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(playContinuousChord), userInfo: nil, repeats: true)
         timer2 = Timer.scheduledTimer(timeInterval: 1.0/4, target: self, selector: #selector(playMelody), userInfo: nil, repeats: true)
-        
     }
     
     
      @objc func playContinuousChord(){
         let lastChord = currentChord
-        
+        kick.play(noteNumber: 100, velocity: 55)
         
         // Invalidate old timers, reinitialize them with new frequencies (IBI)
         if (timerFrequency != newTimerFrequency){
@@ -132,15 +148,10 @@ class  Biomusic {
             timerFrequency = newTimerFrequency
             timer = Timer.scheduledTimer(timeInterval: timerFrequency, target: self, selector: #selector(playContinuousChord), userInfo: nil, repeats: true)
             timer2 = Timer.scheduledTimer(timeInterval: timerFrequency/4, target: self, selector: #selector(playMelody), userInfo: nil, repeats: true)
-            
-            print(" ", timerFrequency)
         }
         
         if (abs(currentTemp - baselineTemp) > deltaTemp){
-            
-            print("current temp     ", currentTemp, "baseline temp     ", baselineTemp)
-            
-            
+            //print("current temp     ", currentTemp, "baseline temp     ", baselineTemp)
             stopChord(note: Notes(rawValue: currentChord)!, scale: .Major)
             
             if (currentTemp > baselineTemp){    //Step up to the next chord in the cycle of fifth
@@ -157,51 +168,59 @@ class  Biomusic {
             scaleIndex = 0
         }
         
+        print(baselineTemp, currentTemp)
+        lastTimeSinceReattack += 1
+        
         if(lastChord != currentChord){
             playChord(note: Notes(rawValue: currentChord)!, scale: .Major)
+            lastTimeSinceReattack = 0
+        }
+            
+        else if (lastTimeSinceReattack == reattackDuration){
+            playChord(note: Notes(rawValue: currentChord)!, scale: .Major)
+            lastTimeSinceReattack = 0
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     func updateTemperature (newTemperature: Double){
         newTemperatureReading = newTemperature
         
-        // Initialize matrix by filling it
-        if (currentTempReadings < readingsNumber){
-            temperatureReadings[currentTempReadings] = newTemperatureReading
-            currentTempReadings += 1
+        // Need to send data for filtering first
+        if (currentTempReadings < temperatureMovingAvgFilter.count){
+            temperatureMovingAvgFilter[currentTempReadings] = newTemperatureReading
         }
         else {
-            // Shift readings matrix to the left and add new value to its end
-            for i in (0...readingsNumber - 2){
-                temperatureReadings[i] = temperatureReadings[i+1]
+            // Shift filter window
+            for i in (0...temperatureMovingAvgFilter.count - 2){
+                temperatureMovingAvgFilter[i] = temperatureMovingAvgFilter[i+1]
             }
-            temperatureReadings[readingsNumber - 1] = newTemperatureReading
+            temperatureMovingAvgFilter[temperatureMovingAvgFilter.count - 1] = newTemperatureReading
         }
+        
+        //Compute Average
+        //print(currentTempReadings,",", newTemperature, ",", computeAvg(A: temperatureMovingAvgFilter))
+        
+        
+        // Update readings matrix when the moving average filter is full
+        if (currentTempReadings > temperatureMovingAvgFilter.count){
+            // Get data from filter
+            let newData = computeAvg(A: temperatureMovingAvgFilter)
+            
+            // If the derivative matrix is not full (still being filled with new values)
+            if (temperatureMatrixIndex < temperatureReadings.count){
+                temperatureReadings[temperatureMatrixIndex] = newData
+                temperatureMatrixIndex += 1
+            }
+            else {
+                // Shift readings matrix to the left and add new value to its end
+                for i in (0...temperatureReadings.count - 2){
+                    temperatureReadings[i] = temperatureReadings[i+1]
+                }
+                temperatureReadings[temperatureReadings.count - 1] = newData
+            }
+            //print(temperatureReadings)
+        }
+        currentTempReadings += 1
     }
     
     
@@ -209,33 +228,48 @@ class  Biomusic {
        //print("The new gsr is \(newGSR)")
         newGSRReading = newGSR
         
-        // Update readings matrix
-        if (currentGSRReadings < readingsNumber){
-            GSRReadings[currentGSRReadings] = newGSRReading
-            currentGSRReadings += 1
+        // Need to send data for filtering first
+        if (currentGSRReadings < GSRMovingAvgFilter.count){
+            GSRMovingAvgFilter[currentGSRReadings] = newGSR
         }
         else {
-            // Shift readings matrix to the left and add new value to its end
-            for i in (0...readingsNumber - 2){
-                GSRReadings[i] = GSRReadings[i+1]
+            // Shift filter window
+            for i in (0...GSRMovingAvgFilter.count - 2){
+                GSRMovingAvgFilter[i] = GSRMovingAvgFilter[i+1]
             }
-            GSRReadings[readingsNumber - 1] = newGSRReading
+            GSRMovingAvgFilter[GSRMovingAvgFilter.count - 1] = newGSR
         }
+        
+        //Compute Average
+        //print(currentGSRReadings,",", newGSR, ",", computeAvg(A: GSRMovingAvgFilter))
+        
+        // Update readings matrix when the moving average filter is full
+        if (currentGSRReadings > GSRMovingAvgFilter.count){
+            // Get data from filter
+            let newData = computeAvg(A: GSRMovingAvgFilter)
+            
+            // If the derivative matrix is not full (still being filled with new values)
+            if (GSRMatrixIndex < GSRReadings.count){
+                GSRReadings[GSRMatrixIndex] = newData
+                GSRMatrixIndex += 1
+            }
+            else {
+                // Shift readings matrix to the left and add new value to its end
+                for i in (0...GSRReadings.count - 2){
+                    GSRReadings[i] = GSRReadings[i+1]
+                }
+                GSRReadings[GSRReadings.count - 1] = newData
+            }
+            //print(GSRReadings)
+        }
+        currentGSRReadings += 1
     }
     
     func updateIBI (newIBI: Double){
         newTimerFrequency = newIBI
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    // The melody is Controled by the second derivative of the GSR
     @objc func playMelody(){
         // Do the second derivative calculation for the temperature
         var ATA = calculateATA(A: quadraticTime, rowsA: 5, columnsA: 3)
@@ -252,6 +286,8 @@ class  Biomusic {
         // Update the current temperature and the current EDA values
         currentTemp = secondDerivativeTemp
         currentEDA = firstDerivativeGSR
+        
+//        print(baselineEDA, currentEDA)
         
         // Determine note to be played based on EDA (or GSR)
         if (abs(currentEDA - baselineEDA) > deltaEDA){
@@ -286,19 +322,19 @@ class  Biomusic {
     // Takes an Int representing a note as an argument
     func playChordNote(note: Int, generator: Int, octave: Int){
         mandolin.fret(noteNumber: note + 12*octave, course: generator)
-        mandolin.pluck(course: generator, position: pluckPosition, velocity: 127)
+        mandolin.pluck(course: generator, position: pluckPosition, velocity: chordsVelocity)
     }
     
     // Takes a note as an argument
     func playMelodyNote(note: Notes, generator: Int, octave: Int){
         mandolin2.fret(noteNumber: note.rawValue + 12*octave, course: generator)
-        mandolin2.pluck(course: generator, position: mandolin2PluckPosition, velocity: 127)
+        mandolin2.pluck(course: generator, position: mandolin2PluckPosition, velocity: melodyVelocity)
     }
     
     // Takes an Int representing a note as an argument
     func playMelodyNote(note: Int, generator: Int, octave: Int){
         mandolin2.fret(noteNumber: note + 12*octave, course: generator)
-        mandolin2.pluck(course: generator, position: mandolin2PluckPosition, velocity: 127)
+        mandolin2.pluck(course: generator, position: mandolin2PluckPosition, velocity: melodyVelocity)
     }
     
     
@@ -329,6 +365,7 @@ class  Biomusic {
         }
     }
     
+    // Helper function to solve the Matrix
     func solveSquareMatrix( A: [[Double]], b:[Double], dimA: Int) -> [Double] {
         var x = [Double](repeating: 0.0, count: dimA)                  // Has the size of RowsA
         
@@ -389,5 +426,14 @@ class  Biomusic {
         }
         return ATb
     }
+    
+    func computeAvg(A: [Double]) -> Double {
+        var sum = 0.0
+        for i in 0...A.count - 1{
+            sum = sum + A[i]
+        }
+        return (sum/A.count)
+    }
+
 }
 
